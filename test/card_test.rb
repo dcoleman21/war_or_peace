4d8becd1fb766b9e2cfbc9f2ae36e7787c67ea17
Dani Coleman <danicoleman00@gmail.com>
2020-05-05 19:29:21 -0600

    Create Deck class test and file; Tested cards method

--- test/card_test.rb
+++ test/card_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 
+
 class CardTest < Minitest::Test
   def setup
     card = Card.new(:diamond, 'Queen', 12)
@@ -9,13 +10,15 @@ class CardTest < Minitest::Test
 
   def test_it_exists
     card = Card.new(:diamond, 'Queen', 12)
+    # card2 = Card.new(:spade, '3', 3)
+    # card3 = Card.new(:heart, 'Ace', 14)
 
     assert_instance_of Card, card
   end
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
