1869614b5535a6942e9314a905d092bc7171b6cd
Dani Coleman <danicoleman00@gmail.com>
2020-05-09 17:04:36 -0600

    Add basic type and winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,43 +10,13 @@ class Turn
   end
 
   def type
-    player1_rank = player1.deck.rank_of_card_at(0)
-    player2_rank = player2.deck.rank_of_card_at(0)
-    if player1_rank == player2_rank
-      determine_war_type
-    else
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
     end
   end
 
-  def determine_war_type
-    player1_card2_rank = player1.deck.rank_of_card_at(2)
-    player2_card2_rank = player2.deck.rank_of_card_at(2)
-    if player1_card2_rank == player2_card2_rank
-      :mutually_assured_destruction
-    else
-      :war
-    end
-  end
-
   def winner
-    player1_rank = player1.deck.rank_of_card_at(0)
-    player2_rank = player2.deck.rank_of_card_at(0)
-    if player1_rank == player2_rank
-      determine_war_winner
-    elsif player1_rank > player2_rank
-      player1
-    else
-      player2
-    end
-  end
-
-  def determine_war_winner
-    player1_card2_rank = player1.deck.rank_of_card_at(2)
-    player2_card2_rank = player2.deck.rank_of_card_at(2)
-    if player1_card2_rank == player2_card2_rank
-      "no winner"
-    elsif player1_card2_rank > player2_card2_rank
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
       player1
     else
       player2
@@ -54,20 +24,10 @@ class Turn
   end
 
   def pile_cards
-    #if it's a basic turn, take top 2 cards of each deck and add to spoils_of_war
-    if type == :basic
-      @spoils_of_war << player1.deck.cards.shift
-      @spoils_of_war << player2.deck.cards.shift
-    elsif type == :war
-      @spoils_of_war << player1.deck.cards.shift(3)
-      @spoils_of_war << player2.deck.cards.shift(3)
-    else
-      player1.deck.cards.shift(3)
-      player2.deck.cards.shift(3)
-    end
+    spoils_of_war << player1.deck.cards.shift
+    spoils_of_war << player2.deck.cards.shift 
+
   end
 
-  def award_spoils(winner)
-    winner.deck.cards.push(*@spoils_of_war)
-  end
+
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -64,25 +64,6 @@ class TurnTest < Minitest::Test
     assert_equal :basic, turn.type
   end
 
-  def test_it_has_a_war_type
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-
-
-    assert_equal :war, turn.type
-  end
-
   def test_it_has_a_basic_winner
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
@@ -119,36 +100,37 @@ class TurnTest < Minitest::Test
     assert_equal [], turn.spoils_of_war
     turn.pile_cards
     assert_equal [card1, card3],turn.spoils_of_war
-    assert_equal [card2, card5, card8], player1.deck.cards
   end
 
-  def test_it_can_pile_cards_into_spoils_of_war_war
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-
-    assert_equal :war, turn.type
-    assert_equal player2, turn.winner
-    turn.pile_cards
-    require "pry"; binding.pry
-    assert_equal 6, turn.spoils_of_war.count
-    # assert_equal [], turn.spoils_of_war
-
-    # assert_equal [card5, card6],turn.spoils_of_war
-    # assert_equal [], player1.deck.cards
-  end
+  # def test_it_can_pile_cards_into_spoils_of_war_war
+  #   skip
+  #   card1 = Card.new(:heart, 'Jack', 11)
+  #   card2 = Card.new(:heart, '10', 10)
+  #   card3 = Card.new(:heart, '9', 9)
+  #   card4 = Card.new(:diamond, 'Jack', 11)
+  #   card5 = Card.new(:heart, '8', 8)
+  #   card6 = Card.new(:diamond, 'Queen', 12)
+  #   card7 = Card.new(:heart, '3', 3)
+  #   card8 = Card.new(:diamond, '2', 2)
+  #   deck1 = Deck.new([card1, card2, card5, card8])
+  #   deck2 = Deck.new([card4, card3, card6, card7])
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #   turn = Turn.new(player1, player2)
+  #
+  #   assert_equal :war, turn.type
+  #   assert_equal player2, turn.winner
+  #   turn.pile_cards
+  #   # require "pry"; binding.pry
+  #   assert_equal 6, turn.spoils_of_war.shift(3)
+  #   # assert_equal [], turn.spoils_of_war
+  #
+  #   # assert_equal [card5, card6],turn.spoils_of_war
+  #   # assert_equal [], player1.deck.cards
+  # end
 
   def test_it_can_award_spoils_basic
+    skip
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
