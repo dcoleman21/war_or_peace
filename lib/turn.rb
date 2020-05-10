745cce7586bb7d0a00e70e2a5865ce72de488e13
Dani Coleman <danicoleman00@gmail.com>
2020-05-10 12:59:43 -0600

    Finish turn class and test

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,20 +12,34 @@ class Turn
   def type
     if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
+    elsif player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
     end
   end
 
   def winner
     if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
       player1
-    else
+    elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
       player2
+    else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      "No Winner"
     end
   end
 
   def pile_cards
-    @spoils_of_war << player1.deck.cards.shift
-    @spoils_of_war << player2.deck.cards.shift
+    if type == :basic
+      @spoils_of_war << player1.deck.cards.shift
+      @spoils_of_war << player2.deck.cards.shift
+    elsif type == :war
+      @spoils_of_war.concat player1.deck.cards.shift(3)
+      @spoils_of_war.concat player2.deck.cards.shift(3)
+    else type == :mutually_assured_destruction
+      player1.deck.cards.shift(3)
+      player2.deck.cards.shift(3)
+    end
   end
 
   def award_spoils(winner)
