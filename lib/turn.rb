d661e81e0205fef13d66a5fcd92000334c2cfc3e
Dani Coleman <danicoleman00@gmail.com>
2020-05-11 08:20:04 -0600

    Re-work type and winner methods. Not resolved

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ class Turn
       :basic
     elsif player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
-    else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
     end
   end
